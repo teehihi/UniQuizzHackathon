6aa8834994bb7cae3d0dd64c558481f3ae5de81e
SaiCT <congtruong19802005@gmail.com>
2025-11-16 02:52:53 +0700

    up

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
-require('dotenv').config(); // Phải ở dòng đầu tiên
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+require("dotenv").config(); // Phải ở dòng đầu tiên
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 // Xóa các import không còn dùng ở đây (multer, mammoth, geminiService, Deck)
 
-const apiRoutes = require('./apiRoutes'); // Import file routes mới
+const apiRoutes = require("./apiRoutes"); // Import file routes mới
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -16,17 +16,18 @@ app.use(express.json());
 // 2. Kết nối MongoDB
 const MONGO_URI = process.env.MONGO_URI;
 if (!MONGO_URI) {
-  throw new Error('Không tìm thấy MONGO_URI trong file .env');
+  throw new Error("Không tìm thấy MONGO_URI trong file .env");
 }
-mongoose.connect(MONGO_URI)
-  .then(() => console.log('Kết nối MongoDB thành công!'))
-  .catch(err => console.error('Lỗi kết nối MongoDB:', err));
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("Kết nối MongoDB thành công!"))
+  .catch((err) => console.error("Lỗi kết nối MongoDB:", err));
 
 // 3. Cấu hình Multer -> Đã chuyển sang apiRoutes.js
 
 // 4. API Endpoints
 // Gắn tất cả các routes từ file apiRoutes vào prefix /api
-app.use('/api', apiRoutes);
+app.use("/api", apiRoutes);
 
 // Tất cả logic app.get, app.post đã được chuyển sang apiRoutes.js
 // Tất cả logic định nghĩa Schema đã được chuyển sang models/FlashcardSet.js
@@ -35,3 +36,20 @@ app.use('/api', apiRoutes);
 app.listen(PORT, () => {
   console.log(`Server đang chạy ở cổng http://localhost:${PORT}`);
 });
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://uniquizzdom.vercel.app/",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+  })
+);
